b9014deccf6458f2babcaf179e8c63bc24c1928e
gokhanKaratass <gokhankaratas650@gmail.com>
2022-07-22 15:13:24 +0300

    Create route structure

--- src/App.js
+++ src/App.js
@@ -1,7 +1,30 @@
 import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
+import Home from './pages/Home'
+
+import Header from './Header/Header'
+
+import DefaultLayout from './layout/DefaultLayout'
+
+import * as ROUTES from './constants/routePath'
 
 function App () {
-  return <div className="home">Merhaba!!</div>
+  return (
+    <BrowserRouter>
+      <Header />
+    <Routes>
+      <Route
+        path={ROUTES.HOME}
+        element={
+          <DefaultLayout>
+            <Home />
+          </DefaultLayout>
+        }
+      />
+    </Routes>
+  </BrowserRouter>
+  )
 }
 
 export default App
