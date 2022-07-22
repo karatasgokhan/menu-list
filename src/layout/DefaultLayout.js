cc8ebb1722e65236bd02f4d3d3f2a9f0da01980c
gokhanKaratass <gokhankaratas650@gmail.com>
2022-07-22 15:13:06 +0300

    Create home and default layout components

---
+++ src/layout/DefaultLayout.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+// eslint-disable-next-line react/prop-types
+export default function DefaultLayout ({ children }) {
+  return (
+    <div className="default-layout-root">
+    <div className="default-layout-wrapper">{children}</div>
+  </div>
+  )
+}
